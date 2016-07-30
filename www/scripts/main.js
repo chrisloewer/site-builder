e68208ea296295d40c5f90d604ac4e6c8c79a9e7
Chris Loewer <chris.loewer@gmail.com>
2016-07-29 23:55:28 -0500

    Menu resizes on scroll.

---
+++ www/scripts/main.js
@@ -0,0 +1,77 @@
+
+
+var header;
+
+// Initial Page Load
+window.addEventListener('load', function () {
+  // Initialize Variables
+  header = document.getElementById('header-nav');
+
+  // Event Listeners
+  document.addEventListener('scroll', headerController, false);
+
+  // Initialize other functionality
+  headerController();
+});
+
+
+// ------------------------------------ HEADER SCROLL RESIZE --------------------------------- //
+
+function headerController() {
+  console.log('header Controller');
+  var breakpoint = 400;
+  var currentPosition = window.scrollY;
+
+  if(currentPosition > breakpoint) {
+    addClass(header, 'mini');
+  }
+  else {
+    removeClass(header, 'mini');
+  }
+}
+
+
+// ------------------------------------ GENERAL UTILITIES ------------------------------------ //
+
+function addClass(element, className) {
+  if(element.classList.contains(className)) {
+    // console.log(className + ' already in classList');
+  }
+  else {
+    element.classList.add(className);
+  }
+}
+
+function removeClass(element, className) {
+  if(element.classList.contains(className)) {
+    element.classList.remove(className);
+  }
+  else {
+    // console.log(className + ' not in classList');
+  }
+}
+
+function toggleClass(element, className) {
+  if(element.classList.contains(className)) {
+    element.classList.remove(className);
+  }
+  else {
+    element.classList.add(className);
+  }
+}
+
+function removeChildNodes(element) {
+  while(element.hasChildNodes()) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+// Adds leading zeros if necessary
+function padInt(int, len){
+  var str = int + '';
+  while (str.length < len) {
+    str = '0' + str;
+  }
+  return str;
+}
+
